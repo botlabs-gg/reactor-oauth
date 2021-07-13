e28247685fa1609613df9d84cbee946dc89f2bdc
Freya Arbjerg <freya@arbjerg.dev>
2021-07-13 21:45:51 +0200

    Fix test dependencies being published

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,10 +21,10 @@ dependencies {
     api("com.github.kittinunf.fuel:fuel:2.3.1")
     api("com.github.kittinunf.fuel:fuel-reactor:2.3.1")
     api("org.json:json:20201115")
-    testImplementation(platform("org.junit:junit-bom:5.7.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("com.github.KennethWussmann:mock-fuel:1.3.0")
-    testImplementation("io.projectreactor:reactor-test:3.4.1")
+    testCompile(platform("org.junit:junit-bom:5.7.0"))
+    testCompile("org.junit.jupiter:junit-jupiter")
+    testCompile("com.github.KennethWussmann:mock-fuel:1.3.0")
+    testCompile("io.projectreactor:reactor-test:3.4.1")
 }
 
 tasks.withType<Test> {
