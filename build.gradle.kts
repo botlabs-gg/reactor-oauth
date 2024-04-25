d81ab77b1f003175e3c89347685f79d3de7400ab
Freya Arbjerg <freya.arbjerg@bluestacks.com>
2024-04-25 09:56:14 +0200

    Update to Gradle 8.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import java.net.URI
 
 plugins {
-    kotlin("jvm") version "1.4.21"
-    id("maven")
+    kotlin("jvm") version "1.9.23"
+    id("maven-publish")
 }
 
 group = "gg.botlabs"
@@ -21,10 +21,10 @@ dependencies {
     api("com.github.kittinunf.fuel:fuel:2.3.1")
     api("com.github.kittinunf.fuel:fuel-reactor:2.3.1")
     api("org.json:json:20201115")
-    testCompile(platform("org.junit:junit-bom:5.7.0"))
-    testCompile("org.junit.jupiter:junit-jupiter")
-    testCompile("com.github.KennethWussmann:mock-fuel:1.3.0")
-    testCompile("io.projectreactor:reactor-test:3.4.1")
+    testImplementation(platform("org.junit:junit-bom:5.7.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("com.github.KennethWussmann:mock-fuel:1.3.0")
+    testImplementation("io.projectreactor:reactor-test:3.4.1")
 }
 
 tasks.withType<Test> {
@@ -36,6 +36,6 @@ tasks.withType<Test> {
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
